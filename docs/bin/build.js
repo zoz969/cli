2a85644059fe9cc8c1f79ebef07a44b6e48ebf28
zoz969 <jihan.barakat2002@gmail.com>
2026-02-12 13:46:53 +0400

    Delete docs directory

--- docs/bin/build.js
+++
@@ -1,9 +0,0 @@
-const run = require('../lib/build.js')
-const { paths } = require('../lib/index')
-
-run(paths)
-  .then((res) => console.error(`Wrote ${res.length} files`))
-  .catch((err) => {
-    process.exitCode = 1
-    console.error(err)
-  })
